2e2a8d4703575dcb007efca7570e486c950d5d28
odogwukelly <odogwukellyice@gmail.com>
2023-11-17 02:52:06 +0100

    hot fix

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,54 +1,38 @@
 #!/usr/bin/python3
+"""reads stdin line by line and computes metrics
+"""
 
-import sys
 
-
-def print_msg(dict_sc, total_file_size):
+def print_stats(filesize, status_stats):
+    """prints stats collected
     """
-    Method to print
-    Args:
-        dict_sc: dict of status codes
-        total_file_size: total of the file
-    Returns:
-        Nothing
+    stats = ""
+    stats += "File size: {}\n".format(filesize)
+    for code in sorted(status_stats):
+        if status_stats[code] > 0:
+            stats += "{}: {}\n".format(code, status_stats[code])
+    print(stats[:-1])
+
+
+if __name__ == '__main__':
+    import sys
+    """main function
     """
-
-    print("File size: {}".format(total_file_size))
-    for key, val in sorted(dict_sc.items()):
-        if val != 0:
-            print("{}: {}".format(key, val))
-
-
-total_file_size = 0
-code = 0
-counter = 0
-dict_sc = {"200": 0,
-           "301": 0,
-           "400": 0,
-           "401": 0,
-           "403": 0,
-           "404": 0,
-           "405": 0,
-           "500": 0}
-
-try:
-    for line in sys.stdin:
-        parsed_line = line.split()  # ✄ trimming
-        parsed_line = parsed_line[::-1]  # inverting
-
-        if len(parsed_line) > 2:
-            counter += 1
-
-            if counter <= 10:
-                total_file_size += int(parsed_line[0])  # file size
-                code = parsed_line[1]  # status code
-
-                if (code in dict_sc.keys()):
-                    dict_sc[code] += 1
-
-            if (counter == 10):
-                print_msg(dict_sc, total_file_size)
-                counter = 0
-
-finally:
-    print_msg(dict_sc, total_file_size)
+    filesize = 0
+    status_codes = {
+            200: 0, 301: 0, 400: 0, 401: 0, 403: 0, 404: 0, 405: 0, 500: 0
+            }
+    i = 1
+    try:
+        for line in sys.stdin:
+            data_list = line.split(" ")
+            status_in = data_list[-2]
+            filesize_in = data_list[-1]
+            if int(status_in) in status_codes:
+                status_codes[int(status_in)] += 1
+            filesize += int(filesize_in)
+            if i % 10 == 0:
+                print_stats(filesize, status_codes)
+            i += 1
+    except KeyboardInterrupt as ki:
+        print_stats(filesize, status_codes)
